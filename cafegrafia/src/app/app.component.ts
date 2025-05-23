0c0ed86075a540df7cc2cfea755aef5adc0d3a7b
JuanJoseMayen <developer05@demosoftonline.com>
2025-05-23 16:19:49 -0600

    Subiendo proyecto derecho

---
+++ cafegrafia/src/app/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'cafegrafia';
+
+  mostrarInformacionGrupo: boolean = false;
+  seccionVisible = false;
+  menuAbierto: boolean = false;
+  modalAbierto: boolean = false;
+selectedEspecialidad: any = null;
+ 
+   especialidades = [
+    {
+      img: 'assets/images/pastel.png',
+      descripcion: 'Pastel individual de chocolate con centro líquido'
+    },
+    {
+      img: 'assets/images/versoHelado.png',
+      descripcion: 'Frappé con vainilla y cacao, servido en vaso decorado con tinta de chocolate'
+    },
+    {
+      img: 'assets/images/tarjetaCafe.png',
+      descripcion: 'En cada pedido, llévate una tarjeta con una recomendación literaria.'
+    }
+  ];
+
+  abrirModal(especialidad: any): void {
+  this.selectedEspecialidad = especialidad;
+  this.modalAbierto = true;
+}
+
+cerrarModal(): void {
+  this.modalAbierto = false;
+  this.selectedEspecialidad = null;
+}
+
+  ngOnInit(): void {
+    this.checkVisibility();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkVisibility();
+  }
+
+   checkVisibility() {
+    const element = document.getElementById('especialidades');
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+      this.seccionVisible = isVisible;
+    }
+  }
+
+  mostrarGrupo(): void {
+    this.mostrarInformacionGrupo = true;
+  }
+
+  mostrarMenu(): void {
+    this.menuAbierto = true;
+  }
+
+  cerrarMensaje(): void {
+    this.mostrarInformacionGrupo = false;
+  }
+}
